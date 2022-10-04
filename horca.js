6d8fbe45c8b6f773e6584b956828a91afbf28cef
Milton Gómez <110268262+Freazy17@users.noreply.github.com>
2022-10-03 21:19:54 -0500

    Initial commit

---
+++ horca.js
@@ -0,0 +1,57 @@
+//Variables
+let palabras = ["ALURA", "ORACLE", "ONE", "JAVASCRIPT", "HTML"];
+let tablero = document.getElementById("horca").getContext("2d");
+let palabraSecreta = "";
+let letras = [];
+let errores = 8;
+
+//Palabra secreta
+function escogerPalabraSecreta() {
+    let palabra = palabras[Math.floor(Math.random() * palabras.length)]
+    palabraSecreta = palabra
+    console.log(palabraSecreta);
+}
+
+function comprobarLetra(key) {
+    let estado = false;
+    if (key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)) {
+        letras.push(key);
+        console.log(key);
+        return estado;
+    } else {
+        estado = true;
+        console.log(key)
+        return estado;
+
+    }
+
+}
+
+function anadirLetraIncorrecta() {
+    errores -= 1;
+    console.log(errores)
+}
+// iniciar juego
+function iniciarJuego() {
+    document.getElementById("iniciar-juego").style.display = "none";
+
+    escogerPalabraSecreta();
+    dibujarCanvas();
+    dibujarLinea();
+
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase();
+        
+        if(comprobarLetra(letra) && palabraSecreta.includes(letra)) {
+            for(let i = 0; i < palabraSecreta.length; i++) {
+                if(palabraSecreta[i] === letra) {
+                    escribirLetraCorrecta(i);
+                }
+            }
+        } else {
+            anadirLetraIncorrecta(letra);
+            escribirLetraIncorrecta(letra, errores);
+        }
+    }
+    
+}
